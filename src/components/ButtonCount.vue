3801c00ad8024588bbc501a83500fff9327d70b3
HsiaoHungWang <wang@ispan.com.tw>
2024-10-08 11:55:14 +0800

    透過 nextTick() 更新 Actual DOM

--- src/components/ButtonCount.vue
+++ src/components/ButtonCount.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { nextTick, reactive, ref } from 'vue';
 
     // const count1 = 0
-    const count = ref(0) //{value:0}
-    const obj = reactive({count:0})
-    console.log(obj)
+    // const count = ref(0) //{value:0}
+    // const obj = reactive({count:0})
+    // console.log(obj)
   //  console.log(count)
 //    {
 //     "dep": {
@@ -17,18 +17,29 @@ import { reactive, ref } from 'vue';
 //     "_value": 1,
  //      "value":1
 // }
-    count.value++;  //{value:1}
-    console.log(count)
+    // count.value++;  //{value:1}
+    // console.log(count)
    
-    const add = ()=>{
+    const count = ref(0) //{value:0}
+    const add = async ()=>{
       count.value++
-      obj.count++
+
+      //強迫馬上更新 Actual DOM
+      await nextTick()
+
+      //找到按鈕讀取按鈕的值
+      const btn = document.querySelector('#button1')
+      console.log(btn.textContent)
+      //obj.count++
     }
 </script>
 
 <template>
     <div>
-<button @click="add">{{ count }}-{{ obj.count }}</button>
+      <button id="button1" @click="add">{{ count }}</button>
+<!--       
+      <button @click="add">{{ count }}-{{ obj.count }}</button> -->
+    
     </div>
 </template>
 
