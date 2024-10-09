ff90657803f6e6c04841b67b12b23449e0b4e2c2
HsiaoHungWang <wang@ispan.com.tw>
2024-10-09 15:26:57 +0800

    v-for + select option

--- src/views/DirectivesView.vue
+++ src/views/DirectivesView.vue
@@ -5,6 +5,14 @@ const message = ref('<p>Hello World!!</p>')
 const isLoading = ref(true)
 const isLoggedIn = ref(true)
 const username = ref('Tom')
+const regions = ref([
+    {"name":"新興區","code":800},
+    {"name":"前金區","code":801},
+    {"name":"苓雅區","code":802},
+    {"name":"鹽埕區","code":803},
+    {"name":"鼓山區","code":804}
+])
+const selectedOption = ref('802')
 
 setTimeout(()=>{
     isLoading.value = false    
@@ -28,6 +36,10 @@ setTimeout(()=>{
         <p>請先 <button  @click="isLoggedIn=!isLoggedIn">登入</button></p>
     </div>
    
+    <select v-model="selectedOption">
+        <option v-for="(region, index) in regions" :key="region.code" :value="region.code">{{index}} {{region.name}}</option>      
+    </select>
+    <span>{{ selectedOption }}</span>
     </div>
 </template>
 
