35aebfc9aaff0ed6ddeca2a9a6e1ec88eb20acbd
HsiaoHungWang <wang@ispan.com.tw>
2024-10-12 11:43:23 +0800

    串接 API - 分類修改

--- src/views/CategoryCRUD.vue
+++ src/views/CategoryCRUD.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
     const BASE_URL = import.meta.env.VITE_API_BASEURL
 
     const API_URL = `${BASE_URL}/Categories` //'https://localhost:7259/api/Categories'
+    const btnText = ref('分類新增')
     const category = ref({
         categoryId:0,
         categoryName:""
@@ -18,9 +19,14 @@ import { ref } from 'vue';
          categories.value = datas         
     }
 
-    //新增分類資料
+    //新增或修改分類資料
     const addCategory = async()=>{
-       // console.log(category.value)
+        if(category.value.categoryId > 0){
+           //修改
+           updateCategory()
+        }else{
+            //新增
+             // console.log(category.value)
        const response = await fetch(API_URL,{
         method:'POST',
         body:JSON.stringify(category.value),
@@ -32,6 +38,8 @@ import { ref } from 'vue';
        }else{
         alert('新增失敗')
        }
+        }
+        clearData()
     }
 
     //刪除分類
@@ -46,6 +54,33 @@ import { ref } from 'vue';
         }
     }
 
+    //修改分類
+    const editCategory = item => {
+category.value = item
+btnText.value ='分類修改'
+    }
+
+    const updateCategory = async() =>{
+        const response = await fetch(`${API_URL}/${category.value.categoryId}`,{
+            method:'PUT',
+            body:JSON.stringify(category.value),
+            headers:{'Content-Type':'application/json'}
+        })
+        if(response.ok){
+            loadCategories()
+        }else{
+            alert('修改失敗')
+        }
+    }
+
+    const clearData = ()=>{
+       btnText.value = '分類新增'
+        category.value = {
+            categoryId:0,
+            categoryName:""
+        }
+    }
+
     loadCategories()
 </script>
 
@@ -57,16 +92,24 @@ import { ref } from 'vue';
             <ul class="list-group">
                 <li v-for="category in categories" :key="category.categoryId" class="list-group-item d-flex justify-content-between align-items-center">
                    {{ category.categoryName }}
-                    <span @click="deleteCategory(category.categoryId)" class="badge text-bg-danger rounded-pill">
-                        <i class="bi bi-trash-fill"></i>
-                    </span>
+
+                   <div>
+                    <span @click="editCategory(category)" class="badge text-bg-warning rounded-pill mx-1"><i class="bi bi-pencil-square"></i></span>
+
+
+<span @click="deleteCategory(category.categoryId)" class="badge text-bg-danger rounded-pill">
+    <i class="bi bi-trash-fill"></i>
+</span>
+                   </div>
+                  
                 </li>         
             </ul>
         </div>
         <div class="col-8">
             <div class="input-group mb-3">
+                <input type="hidden" v-model="category.categoryId" />
        <input type="text" class="form-control" v-model="category.categoryName" />
-       <button @click="addCategory" class="btn btn-primary">新增分類</button>
+       <button @click="addCategory" class="btn btn-primary">{{btnText}}</button>
 </div>
             
         </div>
