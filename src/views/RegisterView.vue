c3ab6c653c30cac7b482f5404647f2c5e8d15676
HsiaoHungWang <wang@ispan.com.tw>
2024-10-12 13:53:55 +0800

    會員註冊含檔案上傳及密碼加密

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,16 +3,16 @@
         <div class="col-3"></div>
         <div class="col-6">
             <h3>會員註冊</h3>
-            <form id="registerForm" novalidate @submit.prevent="validate">
+            <form name="userData" id="registerForm" novalidate @submit.prevent="validate">
                 <div class="input-group">
                     <label for="account" class="input-group-text">帳號</label>
-                    <input type="text" class="form-control" v-model.trim="userData.username" id="account" required autofocus autocomplete="off">
+                    <input type="text" name="Name" class="form-control" v-model.trim="userData.username" id="account" required autofocus autocomplete="off">
                     <span  v-if="!validity.userNameRequired" class="input-group-text bg-danger text-white" ><i class="bi bi-x-lg"></i></span>
                 </div>
                 <div class="mb-3"><small v-if="!validity.userNameRequired" class="text-danger">帳號一定要輸入</small></div>
                 <div class="input-group">
                     <label for="pwd1" class="input-group-text">密碼</label>
-                    <input type="password" class="form-control" v-model.trim="userData.pwd1" id="pwd1" autocomplete="off">
+                    <input type="password" name="Password" class="form-control" v-model.trim="userData.pwd1" id="pwd1" autocomplete="off">
                     <span v-if="!validity.pwdRequired" class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
                 </div>
                 <div class="mb-3"><small v-if="!validity.pwdRequired" class="text-danger">密碼一定要輸入</small></div>
@@ -27,14 +27,22 @@
                 </div>
                 <div class="input-group">
                     <label for="email" class="input-group-text">電子郵件</label>
-                    <input type="email" v-model.trim="userData.useremail" class="form-control" id="email">
+                    <input type="email" name="Email" v-model.trim="userData.useremail" class="form-control" id="email">
                     <span v-if="!validity.emailRequired || !validity.emailFormat" class="input-group-text bg-danger text-white"><i class="bi bi-x-lg"></i></span>
                 </div>
                 <div class="mb-3">
                     <small v-if="!validity.emailRequired" class="text-danger">電子郵件要輸入</small><br>
                     <small v-if="!validity.emailFormat" class="text-danger">電子郵件格式不正確</small>
                 </div>
+                <div class="input-group mb-3">
+                    <label for="age" class="input-group-text">年紀</label>
+                    <input type="number" name="Age"  class="form-control" id="age">
+                   
+                </div>
+                <div class="mb-3">
 
+  <input class="form-control" type="file" name="theFile" id="formFile">
+</div>
                 <button type="submit" class="btn btn-primary" id="buttonSubmit">送出</button>
 
             </form>
@@ -50,7 +58,9 @@
 <script setup>
 import { ref } from 'vue';
 
+const BASE_URL = import.meta.env.VITE_API_BASEURL
 
+const API_URL = `${BASE_URL}/Members` 
 
 const userData = ref({
     "username":"",
@@ -73,7 +83,7 @@ const validity = ref({
 const emailRule = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
 const pwdRule = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
 
-const validate = ()=>{
+const validate = async()=>{
     //解構賦值
     const { username, pwd1, pwd2, useremail } = userData.value
    
@@ -97,8 +107,19 @@ const validate = ()=>{
     validity.value.isValid = validity.value.userNameRequired  && validity.value.pwdRequired && validity.value.emailRequired && validity.value.pwdConfirm && validity.value.emailFormat 
 
     if(validity.value.isValid){
-        //todo 將userData資料透過fecth()傳送到API
-        alert('送出')
+       
+        //alert('送出')
+          //todo 將userData資料透過fecth()傳送到API
+        const formData = new FormData(document.userData)
+        const response = await fetch(API_URL,{
+            method:'POST',
+            body:formData
+        })
+        if(response.ok){
+            alert('註冊成功!!')
+        }
+
+
     }
 }
 
